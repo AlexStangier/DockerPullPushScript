bd119cf6b6da138bbc5fa6fa4624691b07b2adbc
AlexStangier <alex.stangier@pcg.io>
2024-06-28 15:10:18 +0200

    chore/cleanup
    Signed-off-by: AlexStangier <alex.stangier@pcg.io>

--- pull-push-script.sh
+++ pull-push-script.sh
@@ -12,25 +12,22 @@ pull_and_push_image() {
     source_image=$1
     target_image="$target_repository/$source_image"
 
-    # Pull the image from the public repository
     docker pull $source_image
 
-    # Tag the image for the private Artifact Registry
     docker tag $source_image $target_image
 
-    # Push the image to the private Artifact Registry
     docker push $target_image
 
-    # Remove the source and target images from the local machine
     docker rmi $source_image
     docker rmi $target_image
 }
 
 # List of public image repositories to target image names
 images=(
+    # TODO Add more images in the format "public/image:tag target-image:tag"
     "bitnami/cert-manager:latest"
     "nginx:latest"
-    # TODO Add more images in the format "public/image:tag target-image:tag"
+    "chainguard/external-secrets:latest"
 )
 
 # Loop through each image pair and pull and push the images
